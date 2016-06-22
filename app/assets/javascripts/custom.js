df4f25666bbaf25a822e7c67acdc20b3b595d2b7
Kami Boers <kamiboers@gmail.com>
2016-06-21 19:45:03 -0600

    further styling

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    var timer;
+    $("#peak-box").hover(
+        function(){
+             timer = setTimeout(function(){ $('#peak-data').show(400, 'swing'); }, 200);
+        }, function(){
+           clearTimeout(timer);
+           $('#peak-data').hide(500, 'swing');
+        }
+    );
+    $("#med-box").hover(
+        function(){
+             timer = setTimeout(function(){ $('#med-data').show(400, 'swing'); }, 200);
+        }, function(){
+           clearTimeout(timer);
+           $('#med-data').hide(500, 'swing');
+        }
+    );
+    $("#low-box").hover(
+        function(){
+             timer = setTimeout(function(){ $('#low-data').show(400, 'swing'); }, 200);
+        }, function(){
+           clearTimeout(timer);
+           $('#low-data').hide(500, 'swing');
+        }
+    );
+    $("#rest-box").hover(
+        function(){
+             timer = setTimeout(function(){ $('#rest-data').show(400, 'swing'); }, 200);
+        }, function(){
+           clearTimeout(timer);
+           $('#rest-data').hide(500, 'swing');
+        }
+    );
+});
+
+// $('#rest-box').hover(
+//        function(){ $('#rest-data').toggle(500, 'swing'); }
+// );
