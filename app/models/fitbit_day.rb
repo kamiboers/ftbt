7aa59a0dc669999818f3cdee4e519ac413fb522a
Kami Boers <kamiboers@gmail.com>
2016-06-22 01:15:26 -0600

    styling for chart and collapsing divs

--- app/models/fitbit_day.rb
+++ app/models/fitbit_day.rb
@@ -2,7 +2,7 @@ class FitbitDay
   attr_reader :date, :rest, :low, :med, :high, :rest_rate
   
   def initialize(day)
-    @date = day['dateTime']
+    @date = Date.parse(day['dateTime'])
     @rest = day['value']['heartRateZones'].first['caloriesOut'].to_i
     @low = day['value']['heartRateZones'][1]['caloriesOut'].to_i
     @med = day['value']['heartRateZones'][2]['caloriesOut'].to_i
